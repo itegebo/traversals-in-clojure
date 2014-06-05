a6cf11837252c21f748c0dbf3525dd6b50d36497
Ian Tegebo <ian.tegebo@gmail.com>
2014-06-05 16:21:51 -0700

    Remove unnecessary keyword case

--- src/tree_transforms/core.clj
+++ src/tree_transforms/core.clj
@@ -1,19 +1,14 @@
 (ns tree-transforms.core)
 
-;;; helpers
-(defn append-kw [k1 k2]
-  (str (name k1) (name k2)))
-
 ;; traverse thing what how
 
 ;; linear traversal
-(def aline [1 :a "foo"])
+(def aline [1 "foo"])
 
 (defmulti ltrav class)
 (defmethod ltrav java.lang.Long [x] (inc x))
-(defmethod ltrav clojure.lang.Keyword [x] (append-kw x :b))
 (defmethod ltrav java.lang.String [x] (str x "b"))
 (defmethod ltrav clojure.lang.PersistentVector [x] (vec (map ltrav x)))
 
-(assert (= [2 "ab" "foob"]
+(assert (= [2 "foob"]
            (ltrav aline)))
